12f54d1d18fa1294aee4ced934f1cd006def17d4
Martin Borgén <martinb.nymail@gmail.com>
2024-12-07 02:35:23 +0100

    Basic setup for project organization, mockups and build

---
+++ source/day1.c
@@ -0,0 +1,32 @@
+/*  Day 1 of advent of code 2024
+    Martin Borgén
+
+    [B]y holding the two lists up side by side (your puzzle input), it quickly becomes clear
+    that the lists aren't very similar. Maybe you can help The Historians reconcile their lists?
+
+    For example:
+
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+
+    Maybe the lists are only off by a small amount! To find out, pair up the numbers and measure how far apart they are.
+    Pair up the smallest number in the left list with the smallest number in the right list,
+    then the second-smallest left number with the second-smallest right number, and so on.
+
+    To find the total distance between the left list and the right list, add up the distances between all of the pairs you found.
+    In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
+
+    Your actual left and right lists contain many location IDs. What is the total distance between your lists?
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world");
+}
