8da603a2f9a07d2b23ffd58caf81b17dc410618b
Martin Borgén <martinb.nymail@gmail.com>
2024-12-12 11:19:09 +0100

    Sucessfully finds sample result, fails at actual puzzle input

--- source/day7.c
+++ source/day7.c
@@ -36,18 +36,74 @@ In the above example, the sum of the test values for the three equations listed 
 int main()
 {
     FILE *inputs_ptr = fopen(INPUTS_PATH, "r");
+    unsigned long long calibration_result = 0;
 
     char line_buf[LINE_BUF_SIZE];
     while (fgets(line_buf, LINE_BUF_SIZE, inputs_ptr))
     {
-        char token[LINE_BUF_SIZE];
-        strtok(line_buf, ":");
+        bool test_val_found = false;
+        int spaces = 0;
+        struct int_vector nums = int_vector_new();
+        char *token = strtok(line_buf, ": ");
         int test_val = atoi(token);
+        token = strtok(NULL, " ");
         while (token != NULL)
         {
-            strtok(NULL, " ");
+            int num = atoi(token);
+            int_vector_push_back(&nums, num);
+            token = strtok(NULL, " ");
+            spaces++;
+        }
+        spaces--; // the loop always adds one too much
+
+        // permutate operators, from all + to all *
+        for (int i = 1; i <= nums.length; i++)
+        {
+            int sum_1 = nums.values[0];
+            // first, do product of all initial multiplications
+            for (int ii = 1; ii < i; ii++)
+            {
+                sum_1 *= nums.values[ii];
+            }
+
+            // then, only sum, except one multiplication which is incremented through the remaining nums
+            for (int j = i; j <= nums.length; j++)
+            {
+                int sum_2 = sum_1;
+                for (int jj = i; jj < nums.length; jj++)
+                {
+                    if (jj == j)
+                    {
+                        sum_2 *= nums.values[jj];
+                    }
+                    else
+                    {
+                        sum_2 += nums.values[jj];
+                    }
+                }
+
+                if (sum_2 == test_val)
+                {
+                    calibration_result += test_val;
+                    test_val_found = true;
+                    break;
+                }
+            }
+
+            if (test_val_found)
+            {
+                break;
+            }
+            else if (sum_1 == test_val)
+            {
+                calibration_result += test_val;
+                test_val_found = true;
+                break;
+            }
         }
     }
+
+    printf("Part 1. Calibration result: %llu\n", calibration_result);
 
     fclose(inputs_ptr);
 }
