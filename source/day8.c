b77f89a34add53b9b5181739b69a578ce4c6bdd5
Martin Borgén <martinb.nymail@gmail.com>
2024-12-12 22:45:57 +0100

    basic solution to part 1

---
+++ source/day8.c
@@ -0,0 +1,213 @@
+
+
+/*
+https://adventofcode.com/2024/day/8
+
+Scanning across the city, you find that there are actually many such antennas.
+ Each antenna is tuned to a specific frequency indicated by a single lowercase
+ etter, uppercase letter, or digit. You create a map (your puzzle input) of these
+ antennas. For example:
+
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+
+The signal only applies its nefarious effect at specific antinodes based on the
+resonant frequencies of the antennas. In particular, an antinode occurs at any
+point that is perfectly in line with two antennas of the same frequency - but
+only when one of the antennas is twice as far away as the other. This means
+that for any pair of antennas with the same frequency, there are two antinodes,
+one on either side of them.
+
+So, for these two antennas with frequency a, they create the two antinodes
+marked with #:
+
+..........
+...#......
+..........
+....a.....
+..........
+.....a....
+..........
+......#...
+..........
+..........
+
+Adding a third antenna with the same frequency creates several more
+antinodes. It would ideally add four antinodes, but two are off the right
+side of the map, so instead it adds only two:
+
+..........
+...#......
+#.........
+....a.....
+........a.
+.....a....
+..#.......
+......#...
+..........
+..........
+
+Antennas with different frequencies don't create antinodes;
+A and a count as different frequencies. However, antinodes can occur at
+locations that contain antennas. In this diagram, the lone antenna with
+frequency capital A creates no antinodes but has a lowercase-a-frequency
+antinode at its location:
+
+..........
+...#......
+#.........
+....a.....
+........a.
+.....a....
+..#.......
+......A...
+..........
+..........
+
+The first example has antennas with two different frequencies,
+so the antinodes they create look like this, plus an antinode
+overlapping the topmost A-frequency antenna:
+
+......#....#
+...#....0...
+....#0....#.
+..#....0....
+....0....#..
+.#....A.....
+...#........
+#......#....
+........A...
+.........A..
+..........#.
+..........#.
+
+Because the topmost A-frequency antenna overlaps with
+a 0-frequency antinode, there are 14 total unique locations
+that contain an antinode within the bounds of the map.
+
+Calculate the impact of the signal. How many unique locations
+within the bounds of the map contain an antinode?
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "file_reader.h"
+#include "vector.h"
+#include "my_string.h"
+
+#define INPUTS_PATH "../inputs/day8.txt"
+
+int max_int(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int min_int(int a, int b)
+{
+    if (a < b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int main()
+{
+    char *inputs = file_reader(INPUTS_PATH);
+
+    size_t rows_n = strcnt(inputs, "\n");
+    size_t cols_n = strstr(inputs, "\n") - inputs;
+
+    char(*board)[cols_n] = malloc(sizeof(*board) * rows_n);
+
+    char *line = inputs;
+    for (int i = 0; i < rows_n; i++)
+    {
+        memcpy(board[i], line, cols_n);
+        line = strstr(line, "\n") + 1;
+    }
+    free(inputs);
+    inputs = NULL;
+
+    bool(*is_node)[cols_n] = malloc(sizeof(*is_node) * rows_n);
+    for (size_t i = 0; i < rows_n; i++)
+    {
+        for (size_t j = 0; j < cols_n; j++)
+        {
+            is_node[i][j] = false;
+        }
+    }
+
+    unsigned node_count = 0;
+    for (size_t r = 0; r < rows_n; r++)
+    {
+        for (size_t c = 0; c < cols_n; c++)
+        {
+            if (board[r][c] == '.')
+            {
+                continue;
+            }
+
+            size_t left_bound = min_int(c / 2, 0);
+            size_t right_bound = max_int((cols_n - c + 1) / 2, cols_n);
+            size_t top_bound = min_int(r / 2, 0);
+            size_t bottom_bound = max_int((rows_n - r + 1), rows_n);
+
+            for (int i = top_bound; i < bottom_bound; i++)
+            {
+                for (int j = left_bound; j < right_bound; j++)
+                {
+                    if (i == r && j == c)
+                    {
+                        continue;
+                    }
+                    else if (board[i][j] == board[r][c])
+                    {
+                        int delta_row = i - r;
+                        int delta_col = j - c;
+
+                        int node_r = i + delta_row;
+                        int node_c = j + delta_col;
+                        if (node_r >= 0 && node_r < rows_n &&
+                            node_c >= 0 && node_c < cols_n &&
+                            is_node[node_r][node_c] == false)
+                        {
+                            is_node[node_r][node_c] = true;
+                            // printf("node found at %d, %d\n", node_r, node_c);
+                            node_count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    printf("%u\n", node_count);
+
+    free(board);
+    return 0;
+}
